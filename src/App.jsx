f7b280a2bdcdbce7c9666f1b68f69f5cfd52d602
Watcharakorn <watcharakornk2022@gmail.com>
2023-12-12 15:55:33 +0700

    front-end logic

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import f from "./fiboCalFn";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [value, setValue] = useState(0);
+
+  function increment() {
+    setCount((prevState) => prevState + 1);
+  }
+  function decrement() {
+    if (count === 0) return;
+    setCount((prevState) => prevState - 1);
+  }
+
+  useEffect(() => {
+    const fiboNum = f(count);
+    setValue(fiboNum);
+  }, [count]);
+
+  return (
+    <>
+      <h1>Fibonacci Number</h1>
+      <div className="card">
+        <button onClick={decrement}>-</button>
+        <button onClick={increment}>+</button>
+      </div>
+      <div>Number {count}</div>
+      <div>Fibo &apos; s Value is {value}</div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/fiboCalFn.js
@@ -0,0 +1,14 @@
+export default function f(n) {
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+  let result = 0;
+  let f0 = 0;
+  let f1 = 1;
+
+  for (let i = 1; i < n; i++) {
+    result = f0 + f1;
+    f0 = f1;
+    f1 = result;
+  }
+  return result;
+}
